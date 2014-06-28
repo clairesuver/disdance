f8810305fd9d6d19ced265e7649fd603c1186c89
clairesuver <claire.suver@gmail.com>
2014-06-28 13:47:15 -0700

    Receiver toggles an LED when it receives a message

--- sample_ino/receiver_sample/receiver_sample.ino
+++ sample_ino/receiver_sample/receiver_sample.ino
@@ -9,23 +9,38 @@
 
 RH_ASK driver;
 
+int led = 13;
+int voltage = HIGH;
+
 void setup()
 {
     Serial.begin(9600);	// Debugging only
-    if (!driver.init())
+    if (!driver.init()) {
          Serial.println("init failed");
+    }
+    
+    pinMode(led, OUTPUT);
 }
 
 void loop()
 {
     uint8_t buf[RH_ASK_MAX_MESSAGE_LEN];
     uint8_t buflen = sizeof(buf);
-
+    
+    
     if (driver.recv(buf, &buflen)) // Non-blocking
     {
 	int i;
 
 	// Message with a good checksum received, dump it.
 	driver.printBuffer("Got:", buf, buflen);
+
+        digitalWrite(led, voltage);
+        
+        if (voltage == HIGH) {
+          voltage = LOW;
+        } else {
+          voltage = HIGH;
+        }
     }
 }
